6606fb06ccabed97c46b7996c8371b973aef86b9
Jasmina Jovanovic <jasminajancic@gmail.com>
2019-02-07 22:53:36 -0600

    added dataframe for the district summary

--- Pandas/PyCitySchools/main.py
+++ Pandas/PyCitySchools/main.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-
-
 # Dependencies and Setup
 import pandas as pd
 import numpy as np
@@ -15,26 +13,34 @@ student_data_to_load = "Resources/students_complete.csv"
 school_data = pd.read_csv(school_data_to_load)
 student_data = pd.read_csv(student_data_to_load)
 
+student_data.head()
 # Combine the data into a single dataset
 school_data_complete = pd.merge(student_data, school_data, how="left", on=["school_name", "school_name"])
 
 total_schools = school_data.shape[0]
-print (f"There are a total of {total_schools} schools")
 total_students = student_data.shape[0]
-print (f"There are a total of {total_students} students")
 total_budget = school_data['budget'].sum()
-print (f"Total budget for all schools is: {total_budget}")
-print (f"Per student budget is {total_budget/total_students:.2f}")
-print (f"Average math score is {student_data['math_score'].mean():.2f}")
-print (f"Average reading score is {student_data['reading_score'].mean():.2f}")
+total_math_pass = dict((student_data["math_score"]>70).value_counts())[True]
+total_reading_pass = dict((student_data["reading_score"]>70).value_counts())[True]
+average_math_score = student_data['math_score'].mean()
+average_reading_score = student_data['reading_score'].mean()
+
+district_summary = pd.DataFrame(
+    {'Total Schools' : [total_schools],
+    'Total Students' : [total_students],
+    'Total Budget' : [total_budget],
+    'Per Student budget' : [total_budget/total_students],
+    'Average Math Score' : [round(average_math_score, 2)],
+    'Average Reading Score': [round(average_reading_score,2)],
+    '% Students Pass Math' : ['{0:.2%}'.format(total_math_pass/total_students)],
+    '% Students Pass Reading' : ['{0:.2%}'.format(total_reading_pass/total_students)]})
 
 
-spending_bins = [0, 585, 615, 645, 675]
-group_names = ["<$585", "$585-615", "$615-645", "$645-675"]
+district_summary
 
-
-size_bins = [0, 1000, 2000, 5000]
-group_names = ["Small (<1000)", "Medium (1000-2000)", "Large (2000-5000)"]
-
-
-
+# spending_bins = [0, 585, 615, 645, 675]
+# group_names = ["<$585", "$585-615", "$615-645", "$645-675"]
+#
+#
+# size_bins = [0, 1000, 2000, 5000]
+# group_names = ["Small (<1000)", "Medium (1000-2000)", "Large (2000-5000)"]
